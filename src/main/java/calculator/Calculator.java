4a7bc36f8d51563ffd17ba856a5b0fb995004a1a
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-30 19:29:20 +0330

    Update calculator and main functionalities based on changes made in BDD tests.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,32 +4,11 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Enter a value for a: ");
-            double a = scanner.nextDouble();
-
-            System.out.print("Enter a value for b: ");
-            double b = scanner.nextDouble();
-
-            double result = calculateResult(a, b);
-
-            System.out.println("Result: " + result);
-
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter valid values for a and b.");
-        } catch (ArithmeticException e) {
-            System.out.println("Error! Division by zero is not allowed.");
-        }
-    }
-
-    private static double calculateResult(double a, double b) {
+    public int calculateResult(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero is not allowed.");
         }
         double rawResult = Math.sqrt(a / b);
-        return Math.round(rawResult * 1000.0) / 1000.0;
+        return (int) (Math.round(rawResult * 1000.0) / 1000.0);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import calculator.Calculator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a value for a: ");
+            int a = (int) scanner.nextDouble();
+
+            System.out.print("Enter a value for b: ");
+            int b = (int) scanner.nextDouble();
+
+            Calculator calculator = new Calculator();
+            int result = calculator.calculateResult(a, b);
+
+            System.out.println("Result: " + result);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid values for a and b.");
+        } catch (ArithmeticException e) {
+            System.out.println("Error! Division by zero is not allowed.");
+        }
+    }}
